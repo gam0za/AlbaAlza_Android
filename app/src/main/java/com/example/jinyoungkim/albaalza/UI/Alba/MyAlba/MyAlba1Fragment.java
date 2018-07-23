20894d5e413a473bea7de1faee9511eac45e8317
DS-PC219-207\DS <tadpolejjin95@gmail.com>
2018-07-23 18:36:00 +0900

    commit

--- app/src/main/java/com/example/jinyoungkim/albaalza/UI/Alba/MyAlba/MyAlba1Fragment.java
+++ app/src/main/java/com/example/jinyoungkim/albaalza/UI/Alba/MyAlba/MyAlba1Fragment.java
@@ -8,18 +8,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.applikeysolutions.cosmocalendar.view.CalendarView;
 import com.example.jinyoungkim.albaalza.R;
 
 public class MyAlba1Fragment extends Fragment {
+    CalendarView calendar;
 
     public MyAlba1Fragment() {
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_alba1, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_alba1, container, false);
+
+        calendar = (CalendarView)view.findViewById(R.id.calendar_view);
+        setCalendarDesign(calendar);
+
+        return view;
+    }
+
+//    캘린더 디자인 기본 설정
+    public void setCalendarDesign(CalendarView calendar){
+        calendar.setDayTextColor(R.color.calendar_color1); //오늘 날짜 색깔
+        calendar.setSelectedDayBackgroundColor(R.color.maincolor);
+
     }
 
 
